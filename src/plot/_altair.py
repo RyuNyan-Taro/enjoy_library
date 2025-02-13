d332a66d415d3774c9d7cfb81ffb40db710f7a25
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-02-13 09:01:56 +0900

    modify to return vegalite api.

--- src/plot/_altair.py
+++ src/plot/_altair.py
@@ -3,18 +3,20 @@ from vega_datasets import data
 from typing import Optional
 
 
-def plot_sample_data(dataset_name: str, x: Optional[str] = None, y: Optional[str] = None):
+def plot_sample_data(dataset_name: str, x: Optional[str] = None, y: Optional[str] = None) -> alt.vegalite.v5.api.Chart:
 
     _data = getattr(data, dataset_name)()
 
     if x and y:
 
         # make the chart
-        alt.Chart(_data).mark_point().encode(
+        plot_class = alt.Chart(_data).mark_point().encode(
             x=x,
             y=y,
             color='Origin',
         ).interactive()
+
+        return plot_class
 
     else:
         print(_data.info())
